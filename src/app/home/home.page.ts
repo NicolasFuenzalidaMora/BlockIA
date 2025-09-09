d81a53f25a7080ef867d5dc487c2fd1e64262d26
Nicolás Fuenzalida <ni.fuenzalida@duocuc.cl>
2025-09-08 22:49:47 -0300

    primer avance de proyecto block IA

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, MatButtonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [CallNumber]
+})
+export class HomePage {
+  simnumber = '+56997089807';//número de teléfono a marcar
+  statusMessage = '';
+  calling = false;
+  constructor(private callNumber: CallNumber) {}
+
+  makeCall() {
+    this.statusMessage = '📞 Llamando...';
+    this.callNumber.callNumber(this.simnumber, true)
+      .then(res => console.log('La llamada se realizó con éxito', res))
+      .catch(err => console.log('Error al realizar la llamada', err));
+  }
+
+}
