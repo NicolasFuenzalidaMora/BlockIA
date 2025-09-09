e5ed3ed289a6a931bd12679a57dc75afc4255084
Nicolás Fuenzalida <ni.fuenzalida@duocuc.cl>
2025-09-08 23:05:55 -0300

    cambios en tamaño de botón y mensajes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,14 +16,22 @@ import { MatButtonModule } from '@angular/material/button';
 export class HomePage {
   simnumber = '+56997089807';//número de teléfono a marcar
   statusMessage = '';
+  Message = '';
   calling = false;
   constructor(private callNumber: CallNumber) {}
 
   makeCall() {
     this.statusMessage = '📞 Llamando...';
+    this.Message = 'Apertura iniciada'; // Mostrar mensaje al presionar
+    this.calling = true;
     this.callNumber.callNumber(this.simnumber, true)
       .then(res => console.log('La llamada se realizó con éxito', res))
       .catch(err => console.log('Error al realizar la llamada', err));
-  }
 
+    // Limpiar el status después de 15 segundos
+    setTimeout(() => {
+      this.statusMessage = '';
+      this.calling = false;
+    }, 15000);
+  }
 }
